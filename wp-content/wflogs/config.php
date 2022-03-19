f0aa8dea37e8d96441bb7f69a89a4ee9729f3a86
LLFELIPEVV <93674625+LLFELIPEVV@users.noreply.github.com>
2022-03-19 17:01:22 -0500

    Iniciacion del proyecto

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1647985644;s:7:"authKey";s:64:"E%{wrw;Tr!r >{):6#WOv$kG`uQbP#;F;;@Vuypw=v.~p$wg%6j^ApG<}D~^9uEr";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2586;}
